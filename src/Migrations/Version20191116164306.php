d71363afa4245b6f2a4bad886e52e098445832ae
Arnaud Lefevre <arnaud.h.lefevre@gmail.com>
2019-12-25 16:56:12 +0100

    feat(github workflow): fix running unit test

--- src/Migrations/Version20191116164306.php
+++ src/Migrations/Version20191116164306.php
@@ -30,6 +30,7 @@ final class Version20191116164306 extends AbstractMigration
             'CREATE TABLE app_user (id INT NOT NULL, uuid VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITH TIME ZONE NOT NULL, email VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, PRIMARY KEY(id))'
         );
         $this->addSql('COMMENT ON COLUMN app_user.created_at IS \'(DC2Type:datetimetz_immutable)\'');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_88BDF3E9D17F50A6 ON app_user (uuid)');
     }
 
     public function down(Schema $schema): void
