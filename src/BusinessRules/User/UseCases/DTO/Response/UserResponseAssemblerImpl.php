ffc1d563932b78646aabbc6c423e5e7a178f9f3e
arnaud-23 <arnaud.lefevre@openclassrooms.com>
2020-06-01 23:34:49 +0200

    refacto(user): call generic response assembler

--- src/BusinessRules/User/UseCases/DTO/Response/UserResponseAssemblerImpl.php
+++ src/BusinessRules/User/UseCases/DTO/Response/UserResponseAssemblerImpl.php
@@ -2,7 +2,7 @@
 
 namespace App\BusinessRules\User\UseCases\DTO\Response;
 
-use App\BusinessRules\DtoFieldsHydrator;
+use App\BusinessRules\UseCaseResponseAssembler;
 use App\BusinessRules\User\Entities\User;
 use App\BusinessRules\User\Responders\UserResponse;
 use App\BusinessRules\User\Responders\UserResponseAssembler;
@@ -11,9 +11,6 @@ class UserResponseAssemblerImpl implements UserResponseAssembler
 {
     public function create(User $user): UserResponse
     {
-        $response = new UserResponseDTO();
-        DtoFieldsHydrator::hydrate($response, $user);
-
-        return $response;
+        return UseCaseResponseAssembler::create(UserResponseDTO::class, $user);
     }
 }
