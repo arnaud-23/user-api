d24e682caacd08ab2854ce51bb2136dca02f32fd
arnaud-23 <arnaud.lefevre@openclassrooms.com>
2020-06-01 23:48:25 +0200

    refactor(clean-architechture): remove useless use statement in generic assembler

--- src/BusinessRules/UseCaseResponseAssembler.php
+++ src/BusinessRules/UseCaseResponseAssembler.php
@@ -4,10 +4,6 @@ declare(strict_types=1);
 
 namespace App\BusinessRules;
 
-use App\BusinessRules\User\Entities\User;
-use App\BusinessRules\User\Responders\UserResponse;
-use App\BusinessRules\User\UseCases\DTO\Response\UserResponseDTO;
-
 final class UseCaseResponseAssembler
 {
     public static function create(string $responseClassName, $entity): UseCaseResponse
