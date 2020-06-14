24357350d6bde4e728401aa375a9c19d1b924e23
arnaud-23 <arnaud.lefevre@openclassrooms.com>
2020-06-14 18:29:19 +0200

    feat(register-application): add Database migration

---
+++ src/Migrations/Version20200614162819.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200614162819 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'postgresql',
+            'Migration can only be executed safely on \'postgresql\'.'
+        );
+
+        $this->addSql('CREATE SEQUENCE app_application_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql(
+            'CREATE TABLE app_application (id INT NOT NULL, user_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, uuid VARCHAR(255) NOT NULL, PRIMARY KEY(id))'
+        );
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_FDBEFB7DD17F50A6 ON app_application (uuid)');
+        $this->addSql('CREATE INDEX IDX_FDBEFB7DA76ED395 ON app_application (user_id)');
+        $this->addSql(
+            'ALTER TABLE app_application ADD CONSTRAINT FK_FDBEFB7DA76ED395 FOREIGN KEY (user_id) REFERENCES app_user (id) NOT DEFERRABLE INITIALLY IMMEDIATE'
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'postgresql',
+            'Migration can only be executed safely on \'postgresql\'.'
+        );
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE app_application_id_seq CASCADE');
+        $this->addSql('DROP TABLE app_application');
+    }
+}
