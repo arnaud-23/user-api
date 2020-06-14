eaa84069e144575e15ea73b2cd155fec98240a72
arnaud-23 <arnaud.lefevre@openclassrooms.com>
2020-06-14 17:30:21 +0200

    feat(maker): update use case entity to create missing class

--- src/Framework/Component/Maker/Maker/MakeUseCaseEntity.php
+++ src/Framework/Component/Maker/Maker/MakeUseCaseEntity.php
@@ -9,16 +9,28 @@ use Symfony\Bundle\MakerBundle\DependencyBuilder;
 use Symfony\Bundle\MakerBundle\Generator;
 use Symfony\Bundle\MakerBundle\InputConfiguration;
 use Symfony\Bundle\MakerBundle\Maker\AbstractMaker;
-use Symfony\Bundle\MakerBundle\Str;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Question\Question;
 
 final class MakeUseCaseEntity extends AbstractMaker
 {
+    private const SRC_CLASSES_WITH_PACKAGE   = [
+        'Entity'          => 'Entities',
+        'EntityFactory'   => 'Entities',
+        'EntityGateway'   => 'Gateways',
+        'UseCase'         => 'UseCases',
+        'UseCaseRequest'  => 'Requestors',
+        'UseCaseResponse' => 'Responders',
+    ];
+
+    private const TESTS_CLASSES_WITH_PACKAGE = [
+        'InMemoryEntityGateway' => 'Gateways',
+        'UseCaseTest'           => 'UseCases',
+    ];
+
     public static function getCommandName(): string
     {
         return 'make:use-case-entity';
@@ -28,17 +40,20 @@ final class MakeUseCaseEntity extends AbstractMaker
     {
         $nameDescription = 'The name of the use-case class (e.g. <fg=yellow>BlogPost</>)';
         $moduleDescription = 'The name of the module where the entity will be located';
-        $crudTypeDescription = 'The type of use case';
+        $useCasePrefixDescription = 'The prefix will be added in front of the Entity name to create the use case name';
+        $missingClassDescription = 'Select the missing class to generate';
         $command
             ->setDescription('create a new use-case classes')
             ->addArgument('name', InputArgument::OPTIONAL, $nameDescription)
             ->addArgument('module', InputArgument::OPTIONAL, $moduleDescription)
-            ->addArgument('crud-type', InputArgument::OPTIONAL, $crudTypeDescription)
+            ->addArgument('prefix', InputArgument::OPTIONAL, $useCasePrefixDescription)
+            ->addArgument('missing-class', InputArgument::OPTIONAL, $missingClassDescription)
             ->setHelp(file_get_contents(__DIR__ . '/../Resources/help/MakeUseCaseEntity.txt'));
 
         $inputConfig->setArgumentAsNonInteractive('name');
         $inputConfig->setArgumentAsNonInteractive('module');
-        $inputConfig->setArgumentAsNonInteractive('crud-type');
+        $inputConfig->setArgumentAsNonInteractive('prefix');
+        $inputConfig->setArgumentAsNonInteractive('missing-class');
     }
 
     public function interact(InputInterface $input, ConsoleStyle $io, Command $command): void
@@ -59,12 +74,21 @@ final class MakeUseCaseEntity extends AbstractMaker
             $input->setArgument('module', $value);
         }
 
-        if (!$input->getArgument('crud-type')) {
-            $argument = $command->getDefinition()->getArgument('crud-type');
-            $question = new ChoiceQuestion($argument->getDescription(), ['Create']);
+        if (!$input->getArgument('prefix')) {
+            $argument = $command->getDefinition()->getArgument('prefix');
+            $question = new Question($argument->getDescription(), $input->getArgument('prefix'));
             $value = $io->askQuestion($question);
 
-            $input->setArgument('crud-type', $value);
+            $input->setArgument('prefix', $value);
+        }
+
+        if (!$input->getArgument('missing-class')) {
+            $choices = \array_keys(\array_merge(self::SRC_CLASSES_WITH_PACKAGE, self::TESTS_CLASSES_WITH_PACKAGE));
+            $argument = $command->getDefinition()->getArgument('missing-class');
+            $question = new ChoiceQuestion($argument->getDescription(), $choices, null);
+            $value = $io->askQuestion($question);
+
+            $input->setArgument('missing-class', $value);
         }
     }
 
@@ -76,42 +100,86 @@ final class MakeUseCaseEntity extends AbstractMaker
     {
         $entityName = $input->getArgument('name');
         $moduleName = $input->getArgument('module');
-        $crudType = 'Read' === $input->getArgument('crud-type') ? 'Get' : $input->getArgument('crud-type');
+        $prefix = $input->getArgument('prefix');
+        $missingClass = $input->getArgument('missing-class');
 
-        $variables = ['module' => $moduleName, 'entity_name' => $entityName, 'crud_type' => $crudType,];
-        $classesToGenerate = [
-            'Entity' => 'Entities',
-            'EntityFactory' => 'Entities',
-            'EntityGateway' => 'Gateways',
-            'UseCase' => 'UseCases',
-            'UseCaseRequest' => 'Requestors',
-            'UseCaseResponse' => 'Responders',
-            'UseCaseTest' => 'UseCases',
-        ];
-
-        foreach ($classesToGenerate as $class => $package) {
-            $suffix = preg_replace('/^(Entity|UseCase)/', '', $class);
-            $prefix = in_array($package, ['UseCases', 'Requestors', 'UseCases\DTO\Request'])? $crudType : '';
-
-            if ('Test' !== $suffix) {
-                $generator->generateClass(
-                    "App\\BusinessRules\\{$moduleName}\\{$package}\\{$prefix}{$entityName}{$suffix}",
-                    __DIR__ . "/../Resources/skeleton/module/{$class}.tpl.php",
-                    $variables
-                );
-            } else {
-                $generator->generateFile(
-                    "tests/BusinessRules/{$moduleName}/{$package}/{$prefix}{$entityName}{$suffix}.php",
-                    __DIR__ . "/../Resources/skeleton/module/{$class}.tpl.php",
-                    $variables
-                );
-            }
-        }
+        $variables = ['module' => $moduleName, 'entity_name' => $entityName, 'prefix' => $prefix];
+        $this->generateSrcClasses($generator, $prefix, $moduleName, $entityName, $variables, $missingClass);
+        $this->generateTestsClasses($generator, $prefix, $moduleName, $entityName, $variables, $missingClass);
 
         $generator->writeChanges();
 
         $this->writeSuccessMessage($io);
 
         $io->text('Next: Open your use-case and add your code.');
+    }
+
+    private function generateSrcClasses(
+        Generator $generator,
+        string $prefix,
+        string $moduleName,
+        string $entityName,
+        array $variables,
+        ?string $missingClass
+    ): void {
+        foreach ($this->getSrcClassToGenerate($missingClass) as $class => $package) {
+            $prefix = $this->getClassNamePrefix($package, $prefix);
+            $suffix = $this->getClassNameSuffix($class);
+
+            $generator->generateClass(
+                "App\\BusinessRules\\{$moduleName}\\{$package}\\{$prefix}{$entityName}{$suffix}",
+                __DIR__ . "/../Resources/skeleton/module/{$class}.tpl.php",
+                $variables
+            );
+        }
+    }
+
+    private function getSrcClassToGenerate(?string $missingClass): array
+    {
+        return $this->getClassToGenerate($missingClass, self::SRC_CLASSES_WITH_PACKAGE);
+    }
+
+    private function getClassToGenerate(?string $missingClass, array $classesToGenerate): array
+    {
+        if (null !== $missingClass && \array_key_exists($missingClass, $classesToGenerate)) {
+            $classesToGenerate = [$missingClass, $classesToGenerate[$missingClass]];
+        }
+
+        return $classesToGenerate;
+    }
+
+    private function getClassNamePrefix($package, string $default): string
+    {
+        return in_array($package, ['UseCases', 'Requestors']) ? $default : '';
+    }
+
+    private function getClassNameSuffix(string $class): string
+    {
+        return preg_replace('/(Entity|UseCase)/', '', $class);
+    }
+
+    private function generateTestsClasses(
+        Generator $generator,
+        string $prefix,
+        string $moduleName,
+        string $entityName,
+        array $variables,
+        ?string $missingClass
+    ): void {
+        foreach ($this->getTestsClassToGenerate($missingClass) as $class => $package) {
+            $prefix = $this->getClassNamePrefix($package, $prefix);
+            $suffix = $this->getClassNameSuffix($class);
+
+            $generator->generateFile(
+                "tests/BusinessRules/{$moduleName}/{$package}/{$prefix}{$entityName}{$suffix}.php",
+                __DIR__ . "/../Resources/skeleton/module/{$class}.tpl.php",
+                $variables
+            );
+        }
+    }
+
+    private function getTestsClassToGenerate(?string $missingClass): array
+    {
+        return $this->getClassToGenerate($missingClass, self::TESTS_CLASSES_WITH_PACKAGE);
     }
 }
