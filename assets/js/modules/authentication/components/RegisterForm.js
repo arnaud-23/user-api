0c79728b60c7b954c3b56dd2723ff2f46a2ad3a9
arnaud-23 <arnaud.lefevre@openclassrooms.com>
2020-11-29 18:28:41 +0100

    feat(register-form): handle form error

--- assets/js/modules/authentication/components/RegisterForm.js
+++ assets/js/modules/authentication/components/RegisterForm.js
@@ -1,4 +1,4 @@
-import { h, Component } from 'preact';
+import { Component } from 'preact';
 import UserClient from '../hooks/UserClient';
 
 export default class RegisterForm extends Component {
@@ -20,13 +20,28 @@ export default class RegisterForm extends Component {
     }
 
     confirmPassword = event => {
-        let isPasswordIdentique = event.target.value === this.state.password;
-        this.setState({ 'confirmPassword': isPasswordIdentique });
+        let isPasswordEqual = event.target.value === this.state.password;
+        this.setState({ confirmPassword: isPasswordEqual });
     }
 
     onSubmit = async event => {
         event.preventDefault();
         this.setState({ loading: true })
+
+        let fields = ['email', 'password', 'confirm-password'];
+
+        fields.forEach(field => {
+            document.getElementById(field).classList.remove('is-invalid')
+            document.querySelector(`.${field}.invalid-feedback`).textContent = '';
+        })
+
+        if (!this.state.confirmPassword) {
+            document.getElementById('confirm-password').classList.add('is-invalid')
+            document.querySelector('.confirm-password.invalid-feedback').textContent = 'Passwords should not be different';
+            this.setState({ loading: false })
+
+            return;
+        }
 
         let args = {
             email: this.state.email,
@@ -35,13 +50,20 @@ export default class RegisterForm extends Component {
             password: this.state.password,
         }
 
-        await UserClient.post(args);
+        let result = await UserClient.post(args);
+
+        if (result.hasOwnProperty('errors')) {
+            result.errors.forEach(error => {
+                document.getElementById(error.field).classList.add('is-invalid')
+                document.querySelector(`.${error.field}.invalid-feedback`).textContent = error.message;
+            });
+        }
 
         this.setState({ loading: false })
     }
 
     render() {
-        return (<form class="mb-6" onSubmit={this.onSubmit}>
+        return (<form class="mb- needs-validation" onSubmit={this.onSubmit}>
             <fieldset disabled={this.state.loading}>
                 <div class="form-group form-row">
                     <div class="col">
@@ -53,13 +75,15 @@ export default class RegisterForm extends Component {
                 </div>
                 <div class="form-group">
                     <input value={this.state.email} oninput={this.onInput('email')} type="email" class="form-control" id="email" placeholder="name@address.com" required/>
+                    <div class="email invalid-feedback"></div>
                 </div>
 
                 <div class="form-group mb-5">
                     <input value={this.state.password} oninput={this.onInput('password')} type="password" class="form-control" id="password" placeholder="Enter your password" required/>
+                    <div class="password invalid-feedback"></div>
                     <br/>
                     <input oninput={this.confirmPassword} type="password" class="form-control" id="confirm-password" placeholder="Confirm your password" required/>
-                    <div class="invalid-feedback">Must be the same than password</div>
+                    <div class="confirm-password invalid-feedback"></div>
                 </div>
 
                 <button class="btn btn-block btn-primary" type="submit">{this.state.loading ? 'loading...' : 'Sign up'}</button>
